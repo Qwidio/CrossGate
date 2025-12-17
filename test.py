1d49dcafddb8e0d7eb6a00d2cb3da8275a937c7c
Qwidio <qwidqwudpro@gmail.com>
2025-12-17 04:48:04 -1100

    deleting unused script

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-__import__ 
-
-initiated = True
-param = __path__['loc/user/set.crossg']
-
-if initiated == True :
-    print('initiated!')
-    if param == "null":
-        SystemExit
-    else:
-        new_param = "Internal"
-        param = new_param
+# keep for later
